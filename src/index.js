71883c890365f6ceb8dca602bd39e2e2c76692ca
Gustavo <gustavo.athenas13@gmail.com>
2020-07-29 07:26:04 -0300

    Criado rotas do app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/cadastro/Video/Home/App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video'
+
+
+const Pagina404 = () =>(<div>Pagina404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+
+      <Route path="/" component={App} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+      
+    </Switch>
+  </BrowserRouter>,
+
+ // <React.StrictMode>
+ //   <App />
+ // </React.StrictMode>,
   document.getElementById('root')
 );
 
