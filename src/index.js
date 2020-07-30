7be78821a2d796553f4566c76662eb1e805be6da
Gustavo <gustavo.athenas13@gmail.com>
2020-07-29 21:51:41 -0300

    Parte final de roteamento de telas em SPA.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './pages/cadastro/Video/Home/App';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 
 const Pagina404 = () =>(<div>Pagina404</div>)
@@ -15,6 +16,7 @@ ReactDOM.render(
 
       <Route path="/" component={App} exact/>
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/Categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
       
     </Switch>
